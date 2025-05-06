0b70fea95cd64f5ba87a2af77d820670b579513b
Sergei Kopylov <jegvide@gmail.com>
2025-05-07 01:32:57 +0300

    Fourth homework, 11 pts

--- week04_approx_rl/dqn/replay_buffer.py
+++ week04_approx_rl/dqn/replay_buffer.py
@@ -34,17 +34,17 @@ class ReplayBuffer(object):
         for i in idxes:
             data = self._storage[i]
             obs_t, action, reward, obs_tp1, done = data
-            obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
+            obses_t.append(np.asarray(obs_t))
+            actions.append(np.asarray(action))
             rewards.append(reward)
-            obses_tp1.append(np.array(obs_tp1, copy=False))
+            obses_tp1.append(np.asarray(obs_tp1))
             dones.append(done)
         return (
-            np.array(obses_t),
-            np.array(actions),
-            np.array(rewards),
-            np.array(obses_tp1),
-            np.array(dones),
+            np.asarray(obses_t),
+            np.asarray(actions),
+            np.asarray(rewards),
+            np.asarray(obses_tp1),
+            np.asarray(dones),
         )
 
     def sample(self, batch_size):
